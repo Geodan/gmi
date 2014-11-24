2e5cf0b22e122879f6b00c0c05540049fe1d738f
huub <hfl@home.nl>
2014-11-24 10:04:14 +0100

    make a link for GeoExt

---
+++ cgi-bin/hi.py
@@ -0,0 +1,10 @@
+#!d:/Python27/python.exe 
+# -*- coding: UTF-8 -*-
+
+# enable debugging
+import cgitb
+cgitb.enable()
+
+print "Content-Type: text/html;charset=utf-8\n"
+
+print "<h2>Hello World!</h2>"
