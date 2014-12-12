ba65941b038476c3a57ac2aa9a08c8228d772185
huubfleuren <huub.fleuren@geodan.nl>
2014-12-12 07:30:53 -0800

    build in comment in Dockerfile

--- dockerfiles/natuurbrand-web-base/buildgdal.sh
+++
@@ -1,8 +0,0 @@
-
-apt-get install -y build-essential python-all-dev
-wget http://download.osgeo.org/gdal/gdal-1.9.0.tar.gz
-tar xvfz gdal-1.9.0.tar.gz
-cd gdal-1.9.0
-./configure --with-python
-make
-sudo make install
