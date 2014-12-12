3b3afaa643eb463402166c260860a2cdeb164e26
huubfleuren <huub.fleuren@geodan.nl>
2014-12-12 06:06:03 -0800

    nieuw web-base met gdal en ipadress scripting

---
+++ dockerfiles/natuurbrand-web-base/buildgdal.sh
@@ -0,0 +1,8 @@
+
+apt-get install -y build-essential python-all-dev
+wget http://download.osgeo.org/gdal/gdal-1.9.0.tar.gz
+tar xvfz gdal-1.9.0.tar.gz
+cd gdal-1.9.0
+./configure --with-python
+make
+sudo make install
