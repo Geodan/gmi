fafe6e1bc8c6e46eccf885491f486c1227a2bf23
huubfleuren <huub.fleuren@geodan.nl>
2014-12-15 07:00:03 -0800

    rebuild run-images

---
+++ dockerfiles/dbuild.sh
@@ -0,0 +1,8 @@
+#docker build -t web-base ./natuurbrand-web-base
+#docker build -t tomcat ./tomcat7
+docker build -t geoserver ./geoserver-2.6.0
+#docker build -t postgres93 ./postgres-9.3
+docker build -t postgres ./postgres-9.3-run
+#docker build -t apache2 ./apache2
+docker build -t apache ./apache2-run
+
---
+++ dockerfiles/drmi.sh
@@ -0,0 +1,5 @@
+docker rmi postgres
+docker rmi apache
+docker rmi geoserver
+docker images
+
