41977104371116b1c25a649f5a12918df56fa2b8
huubfleuren <huub.fleuren@geodan.nl>
2014-12-16 06:35:42 -0800

    cp libfarsite.so /usr/lib

--- dockerfiles/apache2-run/run.sh
+++ dockerfiles/apache2-run/run.sh
@@ -1,14 +1,9 @@
 #!/bin/sh
 #sed -i s/localhost/"${GEOSERVER_PORT_8080_TCP_ADDR}"/g /etc/apache2/sites-available/000-default.conf
 #sed -i s/8080/"${GEOSERVER_PORT_8080_TCP_PORT}"/g /etc/apache2/sites-available/000-default.conf
-if [ ! -e /date.txt ]
-then
-date > /date.txt
-mkdir -p  /var/www/main/gmi/externals/GeoExt
-git clone https://github.com/geoext/geoext /var/www/main/gmi/externals/GeoExt
-fi
 mkdir -p  /var/data/wildfire/fires
 mkdir -p  /var/data/wildfire/fuelmodels
 chown www-data /var/data/wildfire/fires
 chown www-data /var/data/wildfire/fuelmodels
+cp /var/www/main/gmi/cgi-bin/libfarsite.so /usr/lib/libfarsite.so
 /usr/sbin/apachectl -DFOREGROUND
